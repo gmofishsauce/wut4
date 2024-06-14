204a87c2ccda8fa77cb2b04639180927d7a507fa
gmofishsauce <pdxjjb@gmail.com>
2024-06-14 10:09:58 -0700

    Hopefully-final fix to U10 control line outputs. They are now on the low-order bits and the Arduino reverses the bits in the register. Also pick up the very start of vector support in host software.

--- exer/cex/vector.go
+++ exer/cex/vector.go
@@ -1,1 +1,14 @@
-// B10:0 goes to pin 52, ENA#
+// B10:0 goes to PLCC pin 52, ENA#
+
+package main
+
+import (
+	"fmt"
+)
+
+type TestVector struct {
+}
+
+func ParseVectorFile(fileName string) ([]TestVector, error) {
+	return nil, fmt.Errorf("not implemented")
+}
